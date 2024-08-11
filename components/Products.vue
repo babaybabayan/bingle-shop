25c381102a5171c1918a44c6a306c771028ffae8
abay <akbarputera65@gmail.com>
2024-08-12 01:35:53 +0700

    initial commit

---
+++ components/Products.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-row>
+    <v-col v-for="(product, index) in products" cols="2">
+      <v-card :title="product.title" :ripple="true">
+        <v-card-actions>
+          ini gambar
+          <!-- <v-img
+            :src="require(`@/assets/images/products/${product.thumbnail}`)"
+          >
+          </v-img> -->
+        </v-card-actions>
+        <v-card-text align="center" class="product-title">
+          {{ product.title }}
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          title: 'Asus Zenfone',
+          thumbnail: 'asus-zenfone.png',
+          price: 2000000,
+          categoryId: 1,
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.product-title {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row>
+    <v-col cols="8" class="frame-content"> <products></products> </v-col>
+    <v-col cols="4"> Orders </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style scoped>
+.frame-content {
+  overflow-y: auto;
+}
+</style>
